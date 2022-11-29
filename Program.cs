e3e17479f9b9fd0cf0cbf4f282258781f8b6b4fd
opilane <opilane@HP-CZC6297BBL>
2022-11-29 15:50:47 +0200

    +

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Kino___Cinema
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false); 
             Application.Run(new Form1());
         }
     }
