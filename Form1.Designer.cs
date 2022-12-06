9a62178bee1e625c293a71ce3404da3456cb6390
opilane <opilane@HP-CZC6297BBL>
2022-12-06 15:29:05 +0200

    Updates people, updates...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,15 +26,18 @@
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+        private void InitializeComponent() {            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+}
 
         #endregion
+
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,72 @@ namespace Kino___Cinema
 {
     public partial class Form1 : Form
     {
+        string[] labelTexts = new string[] { "TOP Cinema", "Kava" };
+        int[] fontSizes = new int[] { 30, 15 };
         public SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\opilane\source\repos\Edgar Neverovski TARpv21\Kino\DB\KinoAB.mdf;Integrated Security = True");
         public Form1()
         {
-            InitializeComponent();            
+            this.components = new Container();
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(500, 500);
+            this.Text = "TOP Cinema - Home";
+
+            FlowLayoutPanel menu = new FlowLayoutPanel
+            {
+                Location = new Point(0, 0),
+                Size = new Size(500, 100),
+                BackColor = Color.Black,
+                FlowDirection = FlowDirection.LeftToRight,
+            };
+
+
+
+            
+            for (int i = 0; i < labelTexts.Length; i++)
+            {
+                Label label = new Label
+                {
+                    Font = new Font("SimSun", fontSizes[i], FontStyle.Bold),
+                    ForeColor = Color.Orange,
+                    Text = labelTexts[i],
+                    AutoSize = true,
+                };
+                if (i > 0)
+                {
+                    label.ForeColor = Color.White;
+                }
+                label.Click += Label_Click;
+
+                menu.Controls.Add(label);
+            }
+
+
+
+            
+            this.Controls.Add(menu);
+        }
+
+        void Label_Click(object sender, EventArgs e)
+        {
+            Label pablo = (Label)sender;
+            if (pablo.Text == labelTexts[1])
+            {
+                int x;
+                for (int i = 0; i < 5; i++)
+                {
+                    PictureBox film = new PictureBox
+                    {
+                        Location = new Point(0, 100),
+                        Size = new Size(200, 300),
+                        BackColor = Color.Orange,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+
+                    };
+                    film.Load(@"../../Images/Menu.jpg");
+                    this.Controls.Add(film);
+                }
+
+            }
         }
     }
 }
